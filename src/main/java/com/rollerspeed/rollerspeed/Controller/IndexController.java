2b42f7509343c2757cb84426baa01c73345f4ecd
Dainer <dainer.ramirez@est.iudigital.edu.co>
2025-11-13 18:21:01 -0500

    Producto Mínimo Viable parte1

---
+++ src/main/java/com/rollerspeed/rollerspeed/Controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.rollerspeed.rollerspeed.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    @RequestMapping({ "/home" })
+    public String index() {
+        return "/home/index";
+    }
+
+}
